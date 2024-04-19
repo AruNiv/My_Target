2d44fe6e910477c06410e0cbb0aa1cfb08729054
AruNiv <163409156+AruNiv@users.noreply.github.com>
2024-04-19 11:20:13 +0530

    feat: version and release info added to file release.txt from release.config.js

--- release.config.js
+++ release.config.js
@@ -3,18 +3,15 @@ const config = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    
     ["@semantic-release/git", {
-      "assets": ["*.sh", "package.json"],
+      "assets": ["package.json"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-    }],    
-    [
-      "@semantic-release/exec",
-      {
-        "verifyReleaseCmd": "bash welcome.sh ${nextRelease.version} ${nextRelease.notes}"
-      }
-    ],
-    '@semantic-release/github'
+    }],
+    '@semantic-release/github',
+    ["@semantic-release/exec", {
+      "verifyReleaseCmd": "echo ${nextRelease.version} > release.txt",
+      "generateNotesCmd": "git log -1 --pretty=%B >> release.txt"
+    }]    
   ]
 };
 
